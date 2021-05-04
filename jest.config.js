af22ddbdc10c1f8bf7b37e7b3c8e006b4b98893a
Alexander Böhm <mheob@users.noreply.github.com>
2021-05-04 21:12:18 +0200

    :white_check_mark: Add a first test to fix the ci workflow (#11)

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,13 @@
 process.env.TZ = 'GMT'
 
 module.exports = {
-  roots: ['<rootDir>'],
   preset: 'ts-jest',
   setupFilesAfterEnv: ['<rootDir>/test/setupFilesAfterEnv/index.ts'],
 
   collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}', '!<rootDir>/src/**/*.d.ts'],
   coveragePathIgnorePatterns: ['<rootDir>[/\\\\](cypress|test)[/\\\\]'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
   testPathIgnorePatterns: ['<rootDir>[/\\\\](node_modules|.next|cypress|test)[/\\\\]'],
   transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(ts|tsx)$'],
   transform: {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 
 export const Home = (): JSX.Element => (
-  <div className="container">
+  <div className="container" data-testid="home">
     <Head>
       <title>Create Next App</title>
       <link rel="icon" href="/favicon.ico" />
